910ed445a7c7c42afb35d91fb2156508bec1bfa8
Alexandr Basan <alexandr.basan@gmail.com>
2021-11-19 14:28:20 +1300

    Update ContentView.swift

--- Tip Calc/ContentView.swift
+++ Tip Calc/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var total = "20"
+    @State var tipPercent: Double = 15.0
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Tip Calculator")
+                .font(.title)
+                .bold()
+            
+            HStack {
+                Text("$")
+                TextField("Total", text: $total)
+                    .border(Color.black, width: 0.25)
+            }
             .padding()
+            
+            HStack {
+                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Text("\(Int(tipPercent)) %")
+            }
+            .padding()
+            
+            Text("Tip Ammount: \(total)")
+        }
     }
 }
 
