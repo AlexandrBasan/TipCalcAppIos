20ae00b995c0984a134ccf2e2c7c117999cfdb9c
Alexandr Basan <alexandr.basan@gmail.com>
2021-11-19 13:38:29 +1300

    Initial Commit

---
+++ Tip Calc/Tip_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalcApp.swift
+//  Tip Calc
+//
+//  Created by Alexandr Basan on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
